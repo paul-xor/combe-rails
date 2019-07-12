3502c4b18bdafddfc19769169ce179089dbbad53
Pasha Smolov <pasmo@Pashas-MacBook-Pro.local>
2019-07-11 19:31:13 -0700

    api authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,44 @@
+class SessionsController < ApplicationController
+    #skip_forgery_protection only: [:create]
+    include CurrenUserConcern
+
+    def create
+        
+        user = User
+            .find_by(email: params["user"]["email"])
+            .try(:authenticate, params["user"]["password"])
+
+        if user
+            session[:user_id] = user.id
+            render json: {
+                status: :created,
+                logged_in: true,
+                user: user
+            }
+        else
+            byebug
+            render json: {status: 401}
+        end
+    end
+
+    def logged_in
+        if @current_user
+            render json:{
+                logged_in: true,
+                user: @current_user
+            }
+        else
+            render json:{
+                logged_in: false
+            }
+        end
+    end
+
+    def
+        reset_session
+        render json: { status: 200, logged_in: true}
+    end
+
+end
+
+# curl --header "Content-Type: application/json" --request POST --data '{"user": {"email": "pasha.a.smolov@gmail.com", "password": "123"}}' http://localhost:3000/sessions
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
+  delete :logout, to: 'sessions#logout'
+  get :logget_in, to: 'sessions#logged_in'
   root to: "static#home"
 end
