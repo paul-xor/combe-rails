8d64c57161445e59ce95eda39c3a7679e585d7bd
Pasha Smolov <pasmo@Pashas-MacBook-Pro.local>
2019-07-11 19:39:33 -0700

    update sessions store for production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
+if  Rails.env == 'production'
 Rails.application.config.session_store :cookie_store, key: "_comfe_app", domain: 'pash.comfe.com'
+else
+Rails.application.config.session_store :cookie_store, key: "_comfe_app"
+end
